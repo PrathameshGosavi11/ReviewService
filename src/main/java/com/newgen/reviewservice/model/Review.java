f96230c60cfb4caa29895983c9d2635ceeeefd01
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-03-25 11:12:55 +0530

    add review class here

--- src/main/java/com/newgen/reviewservice/model/Review.java
+++ src/main/java/com/newgen/reviewservice/model/Review.java
@@ -1,4 +1,13 @@
 package com.newgen.reviewservice.model;
 
+import jakarta.persistence.*;
+
+@Entity // tell here this is hibernate manage entity and this class now map to the Db
+@Table(name="reviews")
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
