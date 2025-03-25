ac8c99a536fb4875ecd8b4260bc2b33e8ec574cb
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-03-25 15:07:32 +0530

    add one new API

--- src/main/java/com/newgen/reviewservice/controller/ReviewController.java
+++ src/main/java/com/newgen/reviewservice/controller/ReviewController.java
@@ -3,6 +3,8 @@ package com.newgen.reviewservice.controller;
 import com.newgen.reviewservice.model.Review;
 import com.newgen.reviewservice.service.IReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,9 +24,11 @@ public class ReviewController {
     }
 
     @GetMapping("/{productId}/revies")
-    public List<Review> getAllReviewsByProductId(Long productId)
+    public ResponseEntity<List<Review>> getAllReviewsByProductId(Long productId)
     {
-        return iReviewService.getReviewsByProductId(productId);
+        return ResponseEntity.status(HttpStatus.OK).body (iReviewService.getReviewsByProductId(productId));
     }
+
+    
 
 }
